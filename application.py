fd2c5bcbce66ad47153f23d2b4c4dd1cb35d7a86
Ahmed Nergal <ahmed.nergal@gmail.com>
2018-11-15 22:39:08 +0200

    Final commit, hopefully

--- application.py
+++ application.py
@@ -1,22 +1,32 @@
 import os
+import glob
 import datetime
 
-from flask import Flask, request, jsonify, render_template, redirect, session
+from flask import Flask, request, jsonify, render_template, redirect, flash, url_for, send_from_directory
 from flask_socketio import SocketIO, emit
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "/home/nergal/Programming/Web Development/CS50 Web Programming with Python and Javascript/Projects/project2venv/project2env/project2/uploads"
+ALLOWED_EXTENSIONS = set(["txt", "jpg", "jpeg", "gif", "png", "mp3", "mp4"])
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["JSON_SORT_KEYS"] = False
-socketio = SocketIO(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+socketio = SocketIO(app, binary=True)
 
-users = []
+users_selected_channels = {}
+
 channels = {"general":[]}
+
+def allowed_file(filename):
+    return "." in filename and \
+            filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        print(channels)
-        return render_template("index.html", channels=channels)
+        return render_template("index.html")
     elif request.method == "POST":
         return jsonify({"success":True, "channels":channels})
 
@@ -26,18 +36,21 @@ def register():
         return render_template("register.html")
     elif request.method == "POST":
         displayname = request.form.get("displayname")
-        users.append(displayname)
-        session['displayname'] = displayname
         return redirect("/", code=303)
 
 @socketio.on("user connected")
 def user_connected(data):
     user = data["displayname"]
-    try:
-        selected_channel = data["selected_channel"]
-    except KeyError:
-        selected_channel = None
-    emit("server reply", {"channels":channels}, broadcast=True)
+    if user not in users_selected_channels:
+        users_selected_channels[user] = None
+        selected_channel = users_selected_channels[user]
+    elif user in users_selected_channels:
+        if users_selected_channels[user] == None:
+            selected_channel = None
+        else:
+            selected_channel = users_selected_channels[user]
+
+    emit("server reply", {"channels":channels, "selected_channel":selected_channel})
 
 @socketio.on("create channel")
 def channel(data):
@@ -49,25 +62,72 @@ def channel(data):
 @socketio.on("selected channel")
 def selectedChannel(data):
     selected_channel = data["selected_channel"]
-    print(channels)
-    messages = channels[selected_channel]
-    emit("logged to channel", {"channels":channels, "selected_channel":selected_channel, "messages":messages}, broadcast=True)
+    displayname = data["displayname"]
+    users_selected_channels[displayname] = selected_channel
+    try:
+        messages = channels[selected_channel]
+        emit("logged to channel", {"channels":channels, "selected_channel":selected_channel, "messages":messages})
+    except KeyError:
+        pass
+
 
 @socketio.on("message sent")
 def sendMessage(data):
-    print(channels)
-    print("message")
     displayname = data["displayname"]
-    selected_channel = data["selected_channel"]
+    selected_channel = users_selected_channels[displayname]
     message = data["message"]
     time = datetime.datetime.now()
-    timestamp = time.strftime('%d-%m-%Y %H:%M')
-    print(timestamp)
-    print(type(timestamp))
+    timestamp = time.strftime('%d-%b-%Y %I:%M %p')
     fullMessageData = {"displayname":displayname, "message":message, "timestamp":timestamp}
-    if len(channels[selected_channel]) <= 100:
+    if len(channels[selected_channel]) < 100:
         channels[selected_channel].append(fullMessageData)
-        print(channels)
         emit("message recieved", {"displayname":displayname, "selected_channel":selected_channel, "message":message, "timestamp":timestamp}, broadcast=True)
     else:
-        print("Maximum limit of messages (100 messages) was reached!!!")
+        del channels[selected_channel][0]
+        channels[selected_channel].append(fullMessageData)
+        emit("message recieved", {"displayname":displayname, "selected_channel":selected_channel, "message":message, "timestamp":timestamp}, broadcast=True)
+
+@socketio.on("uploaded file")
+def fileUpload(data):
+    file = data["file"]
+    filename = data["filename"]
+    if allowed_file(filename) == True:
+        filename = secure_filename(filename)
+        #file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        f = open(os.path.join(app.config["UPLOAD_FOLDER"] + '/' + filename), 'wb+')
+        f.write(file)
+        f.close()
+        extension = filename.rsplit('.', 1)[1].lower()
+        if extension == "txt":
+            file_type = "text" + "/" + extension
+        elif extension in ["jpg", "jpeg", "gif", "png"]:
+            file_type = "image" + "/" + extension
+        elif extension == "mp3":
+            file_type == "audio" + "/" + extension
+        elif extension == "mp4":
+            file_type == "video" + "/" + extension
+
+        emit("file recieved", {"filename":filename, "file_type":file_type})
+    else:
+        emit("file not uploaded")
+
+@socketio.on("get files")
+def getFiles():
+    uploaded_files = []
+    os.chdir(UPLOAD_FOLDER)
+    for filename in glob.glob("*"):
+        extension = filename.rsplit(".", 1)[1].lower()
+        if extension == "txt":
+            file_type = "text" + "/" + extension
+        elif extension in ["jpg", "jpeg", "gif", "png"]:
+            file_type = "image" + "/" + extension
+        elif extension == "mp3":
+            file_type == "audio" + "/" + extension
+        elif extension == "mp4":
+            file_type == "video" + "/" + extension
+        uploaded_files.append({"filename":filename, "file_type":file_type})
+    emit("send uploaded files", {"uploaded_files":uploaded_files})
+
+@app.route("/uploads/<path:filename>")
+def downloadFile(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
--- static/index.js
+++ static/index.js
@@ -6,174 +6,273 @@ document.addEventListener("DOMContentLoaded", () => {
     var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
 
     socket.on("connect", () => {
-      displayname = localStorage.getItem("displayname");
-      if (!localStorage.getItem("selected_channel")){
-        const request = new XMLHttpRequest();
-        request.open("POST", "/");
+      const displayname = localStorage.getItem("displayname");
 
-        request.onload = () => {
-          const data = JSON.parse(request.responseText);
-          console.log(data);
-          if (data.success){
-            ViewChannels(data);
-            SelectChannel();
+      const request = new XMLHttpRequest();
+      request.open("POST", "/");
+
+      request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        ViewChannels(data);
         socket.emit("user connected", {"displayname":displayname});
       }
-    }
 
-    request.send();
-  } else {
-        selected_channel = localStorage.getItem("selected_channel");
-        const request = new XMLHttpRequest();
-        request.open("POST", "/");
+      request.send();
 
-        request.onload = () => {
-          console.log(request.responseText);
-          var data = JSON.parse(request.responseText);
-          console.log(data);
-          if (data.success){
-            ViewChannels(data);
-            selected_channel = SelectChannel();
-            try {
-              ViewMessages(data);
+      socket.on("server reply", data => {
+        const displayname = localStorage.getItem("displayname");
+        const selected_channel = data["selected_channel"];
+        ViewSelectedChannel(selected_channel);
+        SelectChannel();
+        ViewMessages(data);
+        socket.emit('selected channel', {"selected_channel":selected_channel, "displayname":displayname});
+        document.querySelector("#channel-submit").onclick = () => {
+            const channel_name = document.querySelector("#channel-name").value;
+            if (channel_name != "" && channel_name != null){
+              document.querySelector("#channel-name").value = "";
+              socket.emit("create channel", {"channel_name":channel_name});
             }
+      }
+    });
 
-            catch (e){
-              if (e instanceof TypeError){
-                
-              }
-            }
-            socket.emit("user connected", {"displayname":displayname, "selected_channel":selected_channel});
+
+      socket.on("channel created", data => {
+        AddChannel(data);
+        selected_channel = SelectChannel();
+        socket.emit('selected channel', {"selected_channel":selected_channel});
+    });
+
+      socket.on("logged to channel", data => {
+        ViewMessages(data);
+        const selected_channel = data["selected_channel"];
+        const displayname = localStorage.getItem("displayname");
+        document.querySelector("#message-submit").onclick = () => {
+          let message = document.querySelector("#message-text").value;
+          if (message != "" && message != null){
+            document.querySelector("#message-text").value = "";
+            socket.emit('message sent', {"displayname":displayname, "message":message, "selected_channel":selected_channel});
           }
         }
-        request.send();
+      });
+
+      socket.on("message recieved", data => {
+        let selected_channel = data["selected_channel"];
+        let selected_channel_dom = document.querySelector(".selected-channel");
+
+        if (selected_channel_dom != null){
+          if (selected_channel_dom.dataset.channelName == selected_channel){
+            chatBox = document.querySelector("#chat-box");
+
+            const displayname = data["displayname"];
+            const messageText = data["message"];
+            const timestamp = data["timestamp"];
+
+            const newMessage = document.createElement("div");
+            newMessage.classList.add("message");
+
+            const displaynameMessageElement = document.createElement("p");
+            displaynameMessageElement.innerHTML = displayname;
+            displaynameMessageElement.classList.add("displayname");
+
+            const messageTextMessageElement = document.createElement("p");
+            messageTextMessageElement.innerHTML = messageText;
+            messageTextMessageElement.classList.add("message-text");
+
+            const timestampMessageElement = document.createElement("p");
+            timestampMessageElement.innerHTML = timestamp;
+            timestampMessageElement.classList.add("timestamp");
+
+            chatBox.appendChild(newMessage);
+
+            newMessage.appendChild(displaynameMessageElement);
+            newMessage.appendChild(messageTextMessageElement);
+            newMessage.appendChild(timestampMessageElement);
+
+            chatBox.scrollTop = chatBox.scrollHeight;
+        }
+
       }
     });
 
-    socket.on("server reply", () => {
-      document.querySelector("#channel-submit").onclick = () => {
-          const channel_name = document.querySelector("#channel-name").value;
-          socket.emit("create channel", {"channel_name":channel_name});
+    // FILE UPLOAD
+
+    document.querySelector("#file-submit").onclick = () => {
+      var file = document.querySelector("#file-input").files[0];
+      filename = file.name;
+      socket.emit("uploaded file", {"file":file, "filename":filename});
     }
-  });
 
+    socket.on("file recieved", data => {
+      alert("File successfully uploaded!!");
+      let fileBox = document.querySelector("#file-box");
+      filename = data["filename"];
+      file_type = data["file_type"];
+      file_size = data["file_size"];
+      var file = document.createElement("div");
+      file.innerHTML= '<strong>File Type:</strong> ' + file_type + '<br> <strong>File Name: </strong> ' +'<a>' + filename + '</a>';
+      file.dataset.fileName = filename;
+      file.classList.add("file");
+      fileBox.appendChild(file);
+      DownloadFile();
+    })
 
-    socket.on("channel created", data => {
-    AddChannel(data);
-    if (!localStorage.getItem("selected_channel")){
-      selected_channel = SelectChannel()
-      console.log(selected_channel)
-      console.log(typeof(selected_channel))
-    } else {
-      SelectChannel()
-      selected_channel = localStorage.getItem("selected_channel");
-      socket.emit('selected channel', {"selected_channel":selected_channel});
+    document.querySelector("#file-getter").onclick = () => {
+      socket.emit("get files");
     }
-  });
 
-    socket.on("logged to channel", data => {
-      ViewMessages(data);
-      const selected_channel = localStorage.getItem("selected_channel");
-      const displayname = localStorage.getItem("displayname");
-      document.querySelector("#message-submit").onclick = () => {
-        message = document.querySelector("#message-text").value;
-        socket.emit('message sent', {"displayname":displayname, "message":message, "selected_channel":selected_channel});
+    socket.on("send uploaded files", data => {
+      uploaded_files = data["uploaded_files"]
+      let fileBox = document.querySelector("#file-box");
+      while (fileBox.firstChild){file_size
+        fileBox.firstChild.remove();
       }
+
+      for (var i = 0; i < uploaded_files.length; i++){
+        filename = uploaded_files[i]["filename"];
+        file_type = uploaded_files[i]["file_type"];
+
+        var file = document.createElement("div");
+        file.innerHTML= '<hr><strong>File Type:</strong> ' + file_type + '<br> <strong>File Name: </strong> ' +'<a>' + filename + '</a>';
+        file.dataset.fileName = filename;
+        file.classList.add("file");
+        fileBox.appendChild(file);
+      }
+      DownloadFile();
     });
 
-    socket.on("message recieved", data => {
-    });
+    socket.on("file not uploaded", () =>{
+      alert("File type not supported!!");
+    })
+
+  });
+
+    function ViewSelectedChannel(selected_channel){
+      document.querySelectorAll(".channel").forEach(channel => {
+        if (channel.dataset.channelName == selected_channel){
+          channel.classList.add("selected-channel");
+          document.querySelector("#channel_name_header").innerHTML = selected_channel;
+        }
+      });
+    }
 
     function SelectChannel(){
       document.querySelectorAll(".channel").forEach(channel => {
-          if (!localStorage.getItem("selected_channel")){
-          } else if (localStorage.getItem("selected_channel") == channel.dataset.channelName) {
-            channel.classList.add("selected-channel");
-          }
         channel.onclick = () => {
           let allChannels = Array.from(channel.parentNode.children);
           allChannels.forEach(childChannel => {
             childChannel.classList.remove("selected-channel");
           });
           const selected_channel = channel.dataset.channelName;
-          console.log(typeof(selected_channel));
           channel.classList.add("selected-channel");
-          localStorage.setItem("selected_channel", selected_channel)
-          socket.emit('selected channel', {"selected_channel":selected_channel});
+          document.querySelector("#channel_name_header").innerHTML = selected_channel;
+          let displayname = localStorage.getItem("displayname");
+          socket.emit('selected channel', {"selected_channel":selected_channel, "displayname":displayname});
         }
       });
     }
+
+    function DownloadFile(){
+      var file_list = document.querySelectorAll(".file");
+      file_list.forEach(file => {
+        let filename = file.dataset.fileName;
+        file.addEventListener("click", () => {
+          const request = new XMLHttpRequest();
+          request.responseType = 'blob';
+          request.open("GET", "/uploads/" + filename, true);
+          request.onload = () => {
+            let downloaded_file = request.response;
+
+            let objectURL = window.URL.createObjectURL(downloaded_file);
+            file_link = file.querySelector("a");
+            file_link.href = objectURL;
+            file_link.target = '_blank';
+            file_link.download = filename;
+          }
+
+          const data = new FormData();
+          data.append("filename", filename);
+          request.send(data);
+          return false;
+        });
+      });
+    }
+
+    function ViewMessages(data){
+      if (data["selected_channel"] != null){
+        let selected_channel = data["selected_channel"]
+
+        messages = data["channels"][selected_channel];
+
+        var chatBox = document.querySelector("#chat-box");
+        while (chatBox.firstChild){
+          chatBox.firstChild.remove();
+        }
+
+        for (var i = 0; i < messages.length; i++){
+
+          chatBox = document.querySelector("#chat-box");
+
+          chatBox.dataset.selectedChannel = selected_channel;
+
+          const displayname = messages[i]["displayname"];
+          const messageText = messages[i]["message"];
+          const timestamp = messages[i]["timestamp"];
+
+          const newMessage = document.createElement("div");
+          newMessage.classList.add("message");
+
+          const displaynameMessageElement = document.createElement("p");
+          displaynameMessageElement.innerHTML = displayname;
+          displaynameMessageElement.classList.add("displayname");
+
+          const messageTextMessageElement = document.createElement("p");
+          messageTextMessageElement.innerHTML = messageText;
+          messageTextMessageElement.classList.add("message-text");
+
+          const timestampMessageElement = document.createElement("p");
+          timestampMessageElement.innerHTML = timestamp;
+          timestampMessageElement.classList.add("timestamp");
+
+          chatBox.appendChild(newMessage);
+
+          newMessage.appendChild(displaynameMessageElement);
+          newMessage.appendChild(messageTextMessageElement);
+          newMessage.appendChild(timestampMessageElement);
+
+          chatBox.scrollTop = chatBox.scrollHeight;
+
+        };
+
+      } else {
+        SelectChannel();
+      }
+    }
+
+    function ViewChannels(data){
+      channels = data["channels"];
+      for (key in channels) {
+        var channelList = document.querySelector("#channels");
+        const newChannel = document.createElement('div');
+        newChannel.className = "channel";
+        const a = document.createElement('a');
+        a.innerHTML = key;
+        newChannel.dataset.channelName = key;
+        newChannel.appendChild(a);
+        channelList.appendChild(newChannel);
+        document.querySelector("#channels").append(newChannel);
+      }
+    }
+
+    function AddChannel(data){
+      let channelList = document.querySelector("#channels");
+      const newChannel = document.createElement('div');
+      newChannel.className = "channel";
+      const a = document.createElement('a');
+      a.innerHTML = data["channel_name"];
+      newChannel.dataset.channelName = data["channel_name"];
+      newChannel.appendChild(a);
+      channelList.appendChild(newChannel);
+      document.querySelector("#channels").append(newChannel);
+    }
+
 });
-
-function ViewMessages(data){
-  selected_channel = localStorage.getItem("selected_channel");
-  messages = data["channels"][selected_channel];
-  console.log(messages)
-
-  var chatBox = document.querySelector("#chat-box");
-  while (chatBox.firstChild){
-    chatBox.firstChild.remove();
-  }
-
-  for (var i = 0; i < messages.length; i++){
-
-    chatBox = document.querySelector("#chat-box");
-
-    chatBox.dataset.selectedChannel = selected_channel;
-
-    const displayname = messages[i]["displayname"];
-    const messageText = messages[i]["message"];
-    const timestamp = messages[i]["timestamp"];
-
-    const newMessage = document.createElement("div");
-    newMessage.classList.add("message");
-
-    const displaynameMessageElement = document.createElement("p");
-    displaynameMessageElement.innerHTML = displayname;
-    displaynameMessageElement.classList.add("displayname");
-
-    const messageTextMessageElement = document.createElement("p");
-    messageTextMessageElement.innerHTML = messageText;
-    messageTextMessageElement.classList.add("message-text");
-
-    const timestampMessageElement = document.createElement("p");
-    timestampMessageElement.innerHTML = timestamp;
-    timestampMessageElement.classList.add("timestamp");
-
-    chatBox.appendChild(newMessage);
-
-    newMessage.appendChild(displaynameMessageElement);
-    newMessage.appendChild(messageTextMessageElement);
-    newMessage.appendChild(timestampMessageElement);
-  };
-
-}
-
-function ViewChannels(data){
-  channels = data["channels"];
-  console.log(channels);
-  for (key in channels) {
-    console.log(key);
-    var channelList = document.querySelector("#channels");
-    const newChannel = document.createElement('div');
-    newChannel.className = "channel";
-    const a = document.createElement('a');
-    a.innerHTML = key;
-    newChannel.dataset.channelName = key;
-    newChannel.appendChild(a);
-    channelList.appendChild(newChannel);
-    document.querySelector("#channels").append(newChannel);
-  }
-}
-
-function AddChannel(data){
-  let channelList = document.querySelector("#channels");
-  const newChannel = document.createElement('div');
-  newChannel.className = "channel";
-  const a = document.createElement('a');
-  a.innerHTML = data["channel_name"];
-  newChannel.dataset.channelName = data["channel_name"];
-  newChannel.appendChild(a);
-  channelList.appendChild(newChannel);
-  document.querySelector("#channels").append(newChannel);
-}
